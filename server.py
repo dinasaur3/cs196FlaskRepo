250327f86a8701c6b628a8661b36e14ff8ee5b36
irdina <irdinasin3@gmail.com>
2018-03-04 10:30:57 -0600

    trying to add onto pages and make form

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask import flask_wtf
+from wtforms import StringField, SubmitField
+
 from flask_socketio import SocketIO, send, join_room, leave_room
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -12,14 +14,17 @@ def home():
 def about():
     return render_template("About.html")
 
-@app.route('/contact', methods = ['POST'])
+@app.route('/contact', methods = ['GET', 'POST'])
 def contact():
-
-    if request.method == 'POST':
-
-    contact = request.form['Name']
-    
-    return "Name: " + contact
+    form = ContactForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        #session sets the borders of a conversation with the database.            
+        db_session.add(user)
+        flash('Thanks you')
+        return redirect(url_for('login'))
+    return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug = True)
