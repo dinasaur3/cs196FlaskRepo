25865f5bb4f50d130fb7b1ab2ed69fb2fcfe04ac
irdina <irdinasin3@gmail.com>
2018-03-04 10:49:16 -0600

    added changes to load new page

--- server.py
+++ server.py
@@ -1,28 +1,22 @@
 from flask import Flask, render_template
-from flask import flask_wtf
-from wtforms import StringField, SubmitField
-
 from flask_socketio import SocketIO, send, join_room, leave_room
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("Home.html")
-
 @app.route('/about')
 def about():
-    return render_template("About.html")
+    return render_template('About.html')
 
-@app.route('/contact', methods = ['GET', 'POST'])
+@app.route('/contact', methods = ['POST'])
 def contact():
     form = ContactForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST':
         user = User(form.username.data, form.email.data,
                     form.password.data)
         #session sets the borders of a conversation with the database.            
         db_session.add(user)
-        flash('Thanks you')
+        contact = request.form['Name']
+        flash('Thank you')
         return redirect(url_for('login'))
     return render_template('contact.html', form=form)
 
